231b82db58963290182108351505a2de478f934d
arn1992 <ratularn2@gmail.com>
2016-11-30 01:05:04 +0600

    Add files via upload

---
+++ part1-number.py
@@ -0,0 +1,51 @@
+Python 3.6.0b3 (default, Nov  1 2016, 03:21:01) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> Python 3.6.0b3 (default, Nov  1 2016, 03:21:01) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 2+2
+4
+>>> 2^3
+1
+>>> 5^3
+6
+>>> 5^1
+4
+>>> 5^2
+7
+>>> 5^3
+6
+>>> 5**2
+25
+>>> 5**3
+125
+>>> "power";
+'power'
+>>> "power"
+'power'
+>>> 'power'
+'power'
+>>> 5**3
+125
+>>> 5*3
+15
+>>> 5+5*9
+50
+>>> tuna=6
+>>> hilsha=8
+>>> tuna*hilsha
+48
+>>> tuna+5
+11
+>>> 4/5
+SyntaxError: invalid syntax
+>>> 5/4
+1.25
+>>> 5//4
+1
+>>> print("round")
+round
+>>> 25/4
+6.25
+>>> 25//4
+6
+>>> 
---
+++ part2-string.py
@@ -0,0 +1,45 @@
+Python 3.6.0b3 (default, Nov  1 2016, 03:21:01) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> "ratul bhai"
+'ratul bhai'
+>>> 'ratul bhai'
+'ratul bhai'
+>>> "i don't think so"
+"i don't think so"
+>>> 'they said,"you are a bad man"'
+'they said,"you are a bad man"'
+>>> 'i don\'t think so'
+"i don't think so"
+>>> print("you are a bad boy")
+you are a bad boy
+>>> print('C:\downloads\nano')
+C:\downloads
+ano
+>>> print(r'C:\downloads\nano')
+C:\downloads\nano
+>>> firstname=aminur
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    firstname=aminur
+NameError: name 'aminur' is not defined
+>>> firstname="aminur"
+>>> lastname="ratul"
+>>> fullname=firstname+" "+lastname
+>>> print(fullname)
+aminur ratul
+>>> fullname+fullname
+'aminur ratulaminur ratul'
+>>> fullname*3
+'aminur ratulaminur ratulaminur ratul'
+>>> fullname+" "+fullname
+'aminur ratul aminur ratul'
+>>> fullname'+" "+' *3
+SyntaxError: invalid syntax
+>>> fullname * 3
+'aminur ratulaminur ratulaminur ratul'
+>>> go="aminur "
+>>> go*5
+'aminur aminur aminur aminur aminur '
+>>> print(go*5)
+aminur aminur aminur aminur aminur 
+>>> 
